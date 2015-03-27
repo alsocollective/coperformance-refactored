4faa22277a134d04a1c0fe4d7cbe4fa519943ce7
and0 <andreiv@gmail.com>
2015-03-27 17:24:16 -0400

    debug test

--- http/assets/js/app/controllers/diagnostics.js
+++ http/assets/js/app/controllers/diagnostics.js
@@ -4,6 +4,9 @@ copofermance.diagnostics = {
 		diagdata: null,
 		tiltEvent: null,
 		data: [],
+		datasets: [],
+		lines: [],
+		paths: [],
 		lastEvent: 0,
 		width: 1800,
 		height: 700,
@@ -14,7 +17,8 @@ copofermance.diagnostics = {
 		linez: null,
 		pathx: null,
 		pathy: null,
-		pathz: null
+		pathz: null,
+		devices: [],
 	},
 	init: function(Socket) {
 		console.log("diagnostics...")
@@ -23,17 +27,25 @@ copofermance.diagnostics = {
 			return false;
 		}
 		copofermance.diagnostics.settings.Socket = Socket;
-
+		copofermance.diagnostics.devices = [null, null];
 
 
 		//used http://bost.ocks.org/mike/path/ for reference
 		//Create the data array... change 60 to the number of points
+
+		copofermance.diagnostics.settings.datasets[0] = [];
+		copofermance.diagnostics.settings.datasets[1] = [];
+
 		for (var a = 0; a < 120; ++a) {
+			var dp = {"x":0, "y": 0, "z": 0};
 			copofermance.diagnostics.settings.data.push({
 				"x": 0,
 				"y": 0,
 				"z": 0
 			})
+			
+			copofermance.diagnostics.settings.datasets[0].push(dp);
+			copofermance.diagnostics.settings.datasets[1].push(dp);
 		}
 
 		copofermance.diagnostics.settings.diagdata = Socket.on("diagdata", copofermance.diagnostics.datain);
@@ -47,34 +59,44 @@ copofermance.diagnostics = {
 
 
 		// LINES ,these determin which data set we use... x,y,z
-		copofermance.diagnostics.settings.linex = d3.svg.line()
+		
+		for (var i =0; i < 2; i++) {
+		var linex = d3.svg.line()
 			.x(function(d, i) {
 				return copofermance.diagnostics.settings.xscale(i);
 			})
 			.y(function(d, i) {
 				return copofermance.diagnostics.settings.yscale(d.x);
 			})
-		copofermance.diagnostics.settings.liney = d3.svg.line()
+
+		copofermance.diagnostics.settings.linex = linex;
+
+		var liney = d3.svg.line()
 			.x(function(d, i) {
 				return copofermance.diagnostics.settings.xscale(i);
 			})
 			.y(function(d, i) {
 				return copofermance.diagnostics.settings.yscale(d.y);
 			})
-		copofermance.diagnostics.settings.linez = d3.svg.line()
+		copofermance.diagnostics.settings.liney= liney;
+
+		var linez = d3.svg.line()
 			.x(function(d, i) {
 				return copofermance.diagnostics.settings.xscale(i);
 			})
 			.y(function(d, i) {
 				return copofermance.diagnostics.settings.yscale(d.z);
 			})
+		copofermance.diagnostics.settings.linez = linez;
 
-
+		copofermance.diagnostics.settings.lines[i] =  {"lx": linex, "ly": liney, "lz" : linez};
+		
+		}
 		//set up the document
 		var margins = 100;
-		var xCol = "#f00";
-		var yCol = "#0f0";
-		var zCol = "#00f";
+		var xCol = ["#f00", "#dfc"];
+		var yCol = ["#0f0", "#f0f"];
+		var zCol = ["#00f", "#27c"];
 		var svg = d3.select("#datalocation").append("svg")
 			.attr("width", copofermance.diagnostics.settings.width + margins)
 			.attr("height", copofermance.diagnostics.settings.height + margins)
@@ -99,61 +121,86 @@ copofermance.diagnostics = {
 		svg = svg.append("g")
 			.attr("transform", "translate(" + margins + "," + margins + ")");
 
+
+		for (var i = 0; i < 2; i++) {
 		// actually add the lines now... and their colours are described in here...
-		copofermance.diagnostics.settings.pathx = svg.append("g")
+		var pathx = svg.append("g")
 			.attr("clip-path", "url(#clip)")
 			.append("path")
-			.datum(copofermance.diagnostics.settings.data)
+			.datum(copofermance.diagnostics.settings.datasets[i])
 			.attr("class", "line")
-			.attr("d", copofermance.diagnostics.settings.linex)
+			.attr("d", copofermance.diagnostics.settings.lines[i].lx)
 			.attr("fill", "none")
-			.attr("stroke", xCol);
+			.attr("stroke", xCol[i]);
 
+		copofermance.diagnostics.settings.pathx = pathx;
 
-
-		copofermance.diagnostics.settings.pathy = svg.append("g")
+		var pathy = svg.append("g")
 			.attr("clip-path", "url(#clip)")
 			.append("path")
-			.datum(copofermance.diagnostics.settings.data)
+			.datum(copofermance.diagnostics.settings.datasets[i])
 			.attr("class", "line")
-			.attr("d", copofermance.diagnostics.settings.liney)
+			.attr("d", copofermance.diagnostics.settings.lines[i].ly)
 			.attr("fill", "none")
-			.attr("stroke", yCol);
+			.attr("stroke", yCol[i]);
+		
+		copofermance.diagnostics.settings.pathy = pathy;
 
-		copofermance.diagnostics.settings.pathz = svg.append("g")
+
+		var pathz = svg.append("g")
 			.attr("clip-path", "url(#clip)")
 			.append("path")
-			.datum(copofermance.diagnostics.settings.data)
+			.datum(copofermance.diagnostics.settings.datasets[i])
 			.attr("class", "line")
-			.attr("d", copofermance.diagnostics.settings.linez)
+			.attr("d", copofermance.diagnostics.settings.lines[i].lz)
 			.attr("fill", "none")
-			.attr("stroke", zCol);
+			.attr("stroke", zCol[i]);
+
+		copofermance.diagnostics.settings.pathz = pathz;
+
+		copofermance.diagnostics.settings.paths[i] = {"px": pathx, "py": pathy, "pz": pathz};
+		}
 	},
 	datain: function(msg) {
-		if (typeof copofermance.diagnostics.settings.data[0] != typeof msg) {
-			console.log("the data type is bad...")
-		} else {
+		if (typeof copofermance.diagnostics.settings.datasets[0][0] != typeof msg) {
+			console.log("the data type is bad...");
+
+		} else if (copofermance.diagnostics.settings.devices.indexOf(msg.id) == -1) {
+		
+			copofermance.diagnostics.settings.devices.push(msg.id);
+		
+		}
+		
+		if (copofermance.diagnostics.settings.devices.length > 2)
+			copofermance.diagnostics.settings.devices.shift();
+
+
+			console.log(copofermance.diagnostics.settings.devices);
+
+			var dex = copofermance.diagnostics.settings.devices.indexOf(msg.id);
 			console.log(msg.id);
+			console.log("DEX "  + dex);
 			msg = msg.data;
 			//add your filter here
 			var filter = msg;
 
 
+
 			//append the new data
-			copofermance.diagnostics.settings.data.push(filter);
+			copofermance.diagnostics.settings.datasets[dex].push(filter);
 
 			//update the graph...
-			copofermance.diagnostics.settings.pathx
-				.attr("d", copofermance.diagnostics.settings.linex)
-			copofermance.diagnostics.settings.pathy
-				.attr("d", copofermance.diagnostics.settings.liney)
-			copofermance.diagnostics.settings.pathz
-				.attr("d", copofermance.diagnostics.settings.linez)
+			copofermance.diagnostics.settings.paths[dex].px
+				.attr("d", copofermance.diagnostics.settings.lines[dex].lx)
+			copofermance.diagnostics.settings.paths[dex].py
+				.attr("d", copofermance.diagnostics.settings.lines[dex].ly)
+			copofermance.diagnostics.settings.paths[dex].pz
+				.attr("d", copofermance.diagnostics.settings.lines[dex].lz)
 
 			//remove the old
-			copofermance.diagnostics.settings.data.shift();
+			copofermance.diagnostics.settings.datasets[dex].shift();
 
-		}
+		
 	},
 	onexit: function(Socket) {
 		window.removeEventListener("devicemotion", copofermance.diagnostics.settings.tiltEvent, true);
@@ -182,15 +229,16 @@ copofermance.diagnostics = {
 				var x = (event.accelerationIncludingGravity.x) ? event.accelerationIncludingGravity.x : 0,
 					y = (event.accelerationIncludingGravity.y) ? event.accelerationIncludingGravity.y : 0,
 					z = (event.accelerationIncludingGravity.z) ? event.accelerationIncludingGravity.z : 0;
-				console.log({
-					"x": x,
-					"y": y,
-					"z": z,
-				});
+				//console.log({
+				//	"x": x,
+				//	"y": y,
+				//	"z": z,
+				//});
 				copofermance.diagnostics.settings.Socket.emit("diagdata", {
 					"x": x,
 					"y": y,
-					"z": z
+					"z": z,
+					"id": Socket.id
 				});
 				copofermance.diagnostics.settings.lastEvent = Date.now();
 			}
