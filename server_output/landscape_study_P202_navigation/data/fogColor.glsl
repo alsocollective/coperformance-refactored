c0a461558bbf52d05f7e0973a01583e133bac682
rekii <elzepplin@gmail.com>
2015-05-14 17:45:09 -0400

    Communication testing

---
+++ server_output/landscape_study_P202_navigation/data/fogColor.glsl
@@ -0,0 +1,15 @@
+#define PROCESSING_COLOR_SHADER
+
+varying vec4 vertColor;
+
+uniform float fogNear;
+uniform float fogFar;
+
+void main(){
+    gl_FragColor = vertColor;
+    
+    vec3 fogColor = vec3(1.0,1.0,1.0);
+    float depth = gl_FragCoord.z / gl_FragCoord.w;
+    float fogFactor = smoothstep(fogNear, fogFar, depth);
+    gl_FragColor = mix(gl_FragColor, vec4(fogColor, gl_FragColor.w), fogFactor);
+}
